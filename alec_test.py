0fb526d4796946af09ab02d5f33e9a31a435e639
Alec-Ben <alec.benedictvt@gmail.com>
2024-04-24 11:56:17 -0400

    no

--- alec_test.py
+++ alec_test.py
@@ -1,1 +1,16 @@
-print('hi')
+import scipy
+import pydub
+import librosa
+import numpy as np
+import matplotlib.pyplot as plt
+import wave
+import pandas
+
+test = wave.Wave_read('lib/acoustic.wav')
+
+num_chanel = test.getnchannels()
+frames = test.getnframes()
+
+all_frames = test.readframes(frames)
+
+
