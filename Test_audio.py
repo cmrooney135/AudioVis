fb195ebce5c9296a2d8cc4aa3403e0eb66d34ad8
Alec-Ben <alec.benedictvt@gmail.com>
2024-05-05 21:10:44 -0400

    no

--- Test_audio.py
+++ Test_audio.py
@@ -5,10 +5,10 @@ from scipy.io.wavfile import write
 sr = 22050
 duration = 60
 
-# chirp signal
-linear_chirp = librosa.chirp(fmin=20,fmax=20000,duration=duration,sr=sr,linear=True)
-linear_chirp /= np.max(np.abs(linear_chirp))
-linear_chirp_int = np.int16(linear_chirp * 32767)
+# linear chirp signal
+chirp = librosa.chirp(fmin=20,fmax=20000,duration=duration,sr=sr,linear=True)
+chirp /= np.max(np.abs(chirp))
+chirp_int = np.int16(chirp * 32767)
 
 # monotone
 mono_tone = librosa.tone(frequency=440,duration=duration,sr=22050) #A
@@ -22,7 +22,7 @@ tone_with_increasing_magnitude /= np.max(np.abs(tone_with_increasing_magnitude))
 tone_int = np.int16(tone_with_increasing_magnitude * 32767)
 
 
-# write('lib/linear_chirp.wav', sr, linear_chirp_int)
+# write('lib/chirp.wav', sr, chirp_chirp_int)
 # write('lib/monotone.wav', sr, mono_tone)
 # write('lib/tone_with_increasing_magnitude.wav', sr, tone_int)
 
